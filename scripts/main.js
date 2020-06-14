524069d7c9e180303312b1ac97ec61a1de708a6b
David <davidpumpkin1@gmail.com>
2020-06-14 00:58:25 -0700

    First commit

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+addContainer(1);
+
+
+
+
+function addContainer(itemContainerNumber) {
+    let itemContainer = document.getElementById( "itemContainer" + itemContainerNumber);
+    let template
+    var = .cloneNode(true);
+
+
+}
