6fdf6e51e62814ab0d0cebc211e775f65c2fde78
David <davidpumpkin1@gmail.com>
2020-06-14 16:11:48 -0700

    finished all html front end

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,25 @@
-addContainer(1);
+for (let o = 1; o <=3; o++) {
+    for (let i = 0; i < 4; i++) {
+        addItem(o);
+    }
+}
+
+addToCart();
+addToCart();
 
 
 
+function addItem(itemContainerNumber) {
+    let template = document.getElementById('itemTemplate');
+    let itemClone = template.content.cloneNode(true);
 
-function addContainer(itemContainerNumber) {
-    let itemContainer = document.getElementById( "itemContainer" + itemContainerNumber);
-    let template
-    var = .cloneNode(true);
+    let itemContainer = document.getElementById( 'itemContainer' + itemContainerNumber);
+    itemContainer.appendChild(itemClone);
+}
+function addToCart() {
+    let template = document.getElementById('cartItem');
+    let itemClone = template.content.cloneNode(true);
 
-
+    let cart = document.getElementById( 'cart' );
+    cart.appendChild(itemClone);
 }
